8ba68aac1f7fb30d04553d1b16c5fe3cbe8ed0f4
Wallace Saturnino <wallacesat@live.co.uk>
2019-10-28 14:58:47 -0300

    Configuração: eslint, prettier, style guide airbnb

---
+++ src/components/RepositoryList/index.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class RepositoryList extends Component {
+  componentDidMount() {}
+
+  render() {
+    return (
+      <ul />
+    );
+  }
+}
