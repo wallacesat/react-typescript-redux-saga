32ec563bb8b45c322faafcc75a5e6bdc2023c0c3
Wallace Saturnino <wallacesat@live.co.uk>
2019-10-28 16:17:21 -0300

    Configuração: Store, Reducers, Actions. Conectando o store na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
+
+import RepositoryList from './components/RepositoryList';
 
 const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <h1>Ola!</h1>
-    </header>
-  </div>
+  <Provider store={store}>
+    <RepositoryList />
+  </Provider>
 );
 
 export default App;
--- src/components/RepositoryList/index.tsx
+++ src/components/RepositoryList/index.tsx
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
+import { bindActionCreators, Dispatch } from 'redux';
+import { connect } from 'react-redux';
+
+import { Repository } from '../../store/ducks/repositories/types';
+import { ApplicationState } from '../../store';
+
+import * as RepositoriesActions from '../../store/ducks/repositories/actions';
+
+interface StateProps {
+  repositories: Repository[]
+}
+
+interface DispatchProps {
+  loadRequest(): void
+}
+
+type Props = StateProps & DispatchProps;
+
+class RepositoryList extends Component<Props> {
+  componentDidMount() {
+    const { loadRequest } = this.props;
 
 
-export default class RepositoryList extends Component {
-  componentDidMount() {}
+    loadRequest();
+  }
 
   render() {
+    const { repositories } = this.props;
+
     return (
-      <ul />
+      <ul>
+        {repositories.map((repo) => (
+          <li>{repo.name}</li>
+        ))}
+      </ul>
     );
   }
 }
+
+const mapStateToProps = (state: ApplicationState) => ({
+  repositories: state.repositories.data,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) => (
+  bindActionCreators(RepositoriesActions, dispatch)
+);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(RepositoryList);
